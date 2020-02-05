e60e95737b811b79c89c9d33e2e889d6c710f2c1
Fernando Caudillo <caudillojavier@gmail.com>
2020-02-05 13:27:42 -0800

    React scaffold ready and express connected to serve react and API calls

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/example', (req, res) => {
+
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+	  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+// Start server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
