008481d7a62426e37a794b7af00aa2da1d77e90c
Luis <luis@Admins-MacBook-Pro.local>
2020-04-09 15:23:38 -0700

    DemoPages

---
+++ client/src/ProfessorSection/ProfessorClasses.js
@@ -0,0 +1,36 @@
+import React  from 'react'; 
+
+
+class ProfessorSection extends React.Component{
+
+    constructor(props){
+        super(props);
+
+       
+
+     
+    }
+
+
+
+
+render(){
+
+    return (
+        <div>
+            <h2>props.name</h2>
+           
+
+        </div>  
+    )
+
+
+
+
+    }
+}
+
+
+export default ProfessorSection; 
+
+
